8050c385584e639acfd2652f346ae621e3b0d074
Hardik Gaur <hardikgaur446@gmail.com>
2026-07-28 13:53:51 +0530

    feat(layout): scaffold application shell

---
+++ src/app/core/layouts/console-layout/console-layout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-console-layout',
+  imports: [],
+  templateUrl: './console-layout.html',
+  styleUrl: './console-layout.scss',
+})
+export class ConsoleLayout {}
